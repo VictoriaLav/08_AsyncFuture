3909923af583a8ae6e7e0097c811451442179bde
Lavitskaya Victoria <nika10by@mail.ru>
2022-05-31 11:22:16 +0300

    Delete android directory

--- android/app/src/main/kotlin/com/example/catch_error/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.catch_error
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/home_modul_7/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.home_modul_7
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
